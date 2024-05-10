80cb5411c24db7f2b7dd70ea3672e7532462a903
Christopher Méndez <mcmchris@hotmail.com>
2024-05-10 16:51:40 -0400

    update shooter

--- shooter.py
+++ shooter.py
@@ -19,16 +19,16 @@ picam2.start_preview(Preview.DRM, x=0, y=0, width=1080, height=1080)
 preview_config = picam2.create_preview_configuration()
 
 #capture_config = picam2.create_still_configuration()
-#picam2.still_configuration.size = (224, 224)
+picam2.still_configuration.size = (224, 224)
 picam2.configure(preview_config)
 
 picam2.start()
 #time.sleep(2)
 
 while(True):
-    print("Press [s] to take a picture...")
+    print("Press [Enter] to take a picture...")
     key = input()
     print("Picture taken")
-    picam2.capture_file('image.'+str(number+1)+'.jpg')
+    picam2.switch_mode_and_capture_file("still",'image.'+str(number+1)+'.jpg')
 
 
