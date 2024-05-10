d0dae1565ef27b3d770dc106ed63f998f03add44
Christopher Méndez <mcmchris@hotmail.com>
2024-05-10 15:40:25 -0400

    shooter added

--- shooter.py
+++ shooter.py
@@ -7,14 +7,14 @@ os.chdir('/root/edge-impulse-mipi-image-uploader/dataset')
 
 images = [i for i in os.listdir(os.getcwd()) if i.lower().startswith('image')]
 if images:
-    newest = max(images,key=os.path.getmtime)
+    newest = max(images, key=os.path.getmtime)
 else:
     newest = 'image0.jpg'
 
 number = int(''.join([i for i in newest if i.isdigit()]))
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
+picam2.start_preview(Preview.DRM, x=0, y=0, width=1920, height=1080)
 
 preview_config = picam2.create_preview_configuration()
 capture_config = picam2.create_still_configuration()
@@ -23,4 +23,5 @@ picam2.configure(preview_config)
 picam2.start()
 time.sleep(2)
 
-picam2.switch_mode_and_capture_file(capture_config, 'test_full'+str(number+1)+'.jpg')
+picam2.switch_mode_and_capture_file(
+    capture_config, 'test_full'+str(number+1)+'.jpg')
