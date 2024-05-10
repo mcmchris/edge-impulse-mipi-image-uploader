ed020306474de6963873d6dd0302bdc0f39a0ac3
Christopher Méndez <mcmchris@hotmail.com>
2024-05-10 17:29:55 -0400

    update shooter

--- shooter.py
+++ shooter.py
@@ -6,11 +6,11 @@ from picamera2 import Picamera2, Preview
 
 os.chdir('/root/edge-impulse-mipi-image-uploader/validate')
 
-images = [i for i in os.listdir(os.getcwd()) if i.lower().startswith('image')]
+images = [i for i in os.listdir(os.getcwd()) if i.lower().startswith('no anomaly')]
 if images:
     newest = max(images, key=os.path.getmtime)
 else:
-    newest = 'image0.jpg'
+    newest = 'no anomaly.jpg'
 
 number = int(''.join([i for i in newest if i.isdigit()]))
 
@@ -25,7 +25,7 @@ picam2.configure(preview_config)
 picam2.start()
 time.sleep(2)
 
-picam2.switch_mode_and_capture_file("still", 'image.'+str(number+1)+'.jpg')
+picam2.switch_mode_and_capture_file("still", 'no anomaly.'+str(number+1)+'.jpg')
 
 #while(True):
 #    print("Press [s] to take a picture...")
