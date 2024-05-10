50107cea5d5ada3c7ee33af510347452868400f0
Christopher Méndez <mcmchris@hotmail.com>
2024-05-10 16:03:09 -0400

    shooter added

--- shooter.py
+++ shooter.py
@@ -14,15 +14,15 @@ else:
 number = int(''.join([i for i in newest if i.isdigit()]))
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.DRM, x=0, y=0, width=1920, height=1080)
+picam2.start_preview(Preview.DRM, x=0, y=0, width=1080, height=1080)
 
 preview_config = picam2.create_preview_configuration()
-#capture_config = picam2.create_still_configuration(size=(224, 224))
-capture_config = picam2.still_configuration.size = (224, 224)
+#capture_config = picam2.create_still_configuration()
+picam2.still_configuration.size = (224, 224)
 picam2.configure(preview_config)
 
 picam2.start()
 time.sleep(2)
 
 picam2.switch_mode_and_capture_file(
-    capture_config, 'image'+str(number+1)+'.jpg')
+    "still", 'image'+str(number+1)+'.jpg')
